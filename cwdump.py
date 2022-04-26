c04cd972d754197f2bc997e58af4e82a904d2c94
dvntx <67381633+dvntx@users.noreply.github.com>
2022-04-26 21:50:38 +0100

    lang/rank .mds

--- cwdump.py
+++ cwdump.py
@@ -1,27 +1,72 @@
 import requests
 import os
+from selenium import webdriver
 
 def main():
   if not os.path.exists('generated'):
     os.makedirs('generated')
 
-  #username = input('Username: ')
-  username = "dvnt"
+  driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
+  driver.get("https://www.codewars.com/users/sign_in")
 
-  r = requests.get(f"https://www.codewars.com/api/v1/users/{username}")
-  data = r.json()
+  input("Please login on the browser and press enter to continue...")
 
-  langs = [x for x in data['ranks']['languages']]
+  username = driver.find_element_by_xpath("/html/body/div[1]/div[1]/header/ul/li[4]/div/div/ul/li[1]/a").get_attribute("href")
+  username = username[31:]
+  print(username)
 
-  totalCompleted = data["codeChallenges"]["totalCompleted"]
+  driver.get("https://www.codewars.com/users/dvnt/completed_solutions")
 
-  page_count = abs(totalCompleted - 1) // 200
+  profile = requests.get(f"https://www.codewars.com/api/v1/users/{username}").json()
 
-  while page_count >= 0:
-    chals = requests.get(f"https://www.codewars.com/api/v1/users/{username}/code-challenges/completed?page={page_count}").json()
-    for chal in chals["data"]:
-      print(chal)
-    page_count -= 1
+  langs = [x for x in profile['ranks']['languages']]
+  for lang in langs:
+    if not os.path.exists(f"generated/{lang}"):
+      os.makedirs(f"generated/{lang}")
+  
+
+  totalCompleted = profile["codeChallenges"]["totalCompleted"]
+
+  index = 0
+  while index < totalCompleted:
+    index += 1
+    base = f"/html/body/div[1]/div[1]/main/div[5]/div/div/div[2]/div[{index}]"
+    chalName = driver.find_element_by_xpath(f"{base}/div[1]/a").text
+    chalURL = driver.find_element_by_xpath(f"{base}/div[1]/a").get_attribute("href")
+    
+    challenge = requests.get(f"https://www.codewars.com/api/v1/code-challenges/{chalURL[30:]}").json()
+
+    print(challenge)
+    input()
+    try:
+      creator = challenge["createdBy"]["username"]
+    except:
+      creator = "Unknown"
+    input(creator)
+    rank = challenge["rank"]["name"]
+    input(rank)
+    desc = challenge["description"]
+    tags = challenge["tags"]
+    tags = "` `".join(tags)
+    tags += "`"
+    tags = "`" + tags
+    input(tags)
+
+    lang = driver.find_element_by_xpath(f"{base}/h6").text.lower()[:-1]
+    code = driver.find_element_by_xpath(f"{base}/div[2]/pre/code").text
+
+    solution = f"""```{lang}
+{code}
+```"""
+    with open(f"generated/{lang}/{rank}.md", "a") as f:
+      f.write(f"# [{chalName}]({chalURL})\nby [{creator}](https://www.codewars.com/users/{creator})\n## Description\n{desc}\n## Solution:\n{solution}\n###\nTags: {tags}\n<br>\n")
+
+    input()
+
+
+
+
+  
 
 if __name__ == "__main__":
   main()
